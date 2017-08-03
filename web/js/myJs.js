1d2b554b8f4615cf9cddca8a4e35bc92081535e1
algostructures <saurabh945644@gmail.com>
2017-08-03 13:51:45 +0530

    Making it Web Project

---
+++ web/js/myJs.js
@@ -0,0 +1,13 @@
+/**
+ * Created by prajapas on 8/3/2017.
+ */
+
+/*Menu-toggle*/
+
+$(document).ready(function () {
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("active");
+        //alert(1);
+    });
+});
